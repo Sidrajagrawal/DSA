c6367b420b9eeb497c2062b0f5cb1cefe5592f18
sidra <sidrajagrawal@gmail.com>
2025-07-21 15:06:01 +0530

    Conversion Operation

---
+++ src/com/Stack/PostfixToPrefix.java
@@ -0,0 +1,27 @@
+package com.Stack;
+import java.util.*;
+
+public class PostfixToPrefix {
+	
+	private static String Convert(String st) {
+		Stack<String> stack = new Stack<>();
+		
+		for(int i=0;i<st.length();i++) {
+			char ch = st.charAt(i);
+			if(Character.isLetterOrDigit(ch)) {
+				stack.push(""+ch);
+			}else {
+				String t1 = stack.pop();
+				String t2 = stack.pop();
+				stack.push(ch+t2+t1);
+			}
+		}
+		return stack.peek();
+	}
+	
+	public static void main(String args[]) {
+			String st = "AB-DE+F*/";
+			System.out.println("Postfix Expression: "+st);
+			System.out.println("Prefix Expression: "+Convert(st));
+	}
+}
