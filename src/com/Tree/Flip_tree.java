4d860e5836392298d3ac5d38a1d43876e1de9b08
sidra <sidrajagrawal@gmail.com>
2025-08-08 02:11:51 +0530

    New

--- src/com/Tree/Flip_tree.java
+++ src/com/Tree/Flip_tree.java
@@ -1,5 +1,21 @@
 package com.Tree;
 
 public class Flip_tree {
-
+	
+	  public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	  }
+	 
+	class Solution {
+	    
+	}
 }
---
+++ src/com/Tree/minimum_depth_of_binary_tree.java
@@ -0,0 +1,31 @@
+package com.Tree;
+import java.util.*;
+
+public class minimum_depth_of_binary_tree {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+		public int minDepth(TreeNode root) {
+			Queue<TreeNode> q = new L
+		}
+	}
+
+}
