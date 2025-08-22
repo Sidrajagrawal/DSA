89a86f9de50dca7566ee4b2b6dadaf90c34fbce3
sidra <sidrajagrawal@gmail.com>
2025-08-22 08:22:08 +0530

    NEw

--- src/com/Tree/Diameter_Binary_Tree_M1.java
+++ src/com/Tree/Diameter_Binary_Tree_M1.java
@@ -1,6 +1,6 @@
 package com.Tree;
 
-public class Practice_tree {
+public class Diameter_Binary_Tree_M1 {
 	public class TreeNode {
 		int val;
 		TreeNode left;
@@ -19,16 +19,28 @@ public class Practice_tree {
 			this.right = right;
 		}
 	}
+
 	public static int height(TreeNode root) {
-		if(root == null) {
+		if (root == null) {
 			return -1;
 		}
 		int lh = height(root.left);
 		int rh = height(root.right);
-		return Math.max(rh,lh)+1;
+		return Math.max(rh, lh) + 1;
 	}
+
 	public static int diameter(TreeNode root) {
 		return findDiameter(root);
 	}
 
+	private static int findDiameter(TreeNode root) {
+		// TODO Auto-generated method stub
+		if (root == null) {
+			return 0;
+		}
+		int ld = findDiameter(root.left);
+		int rd = findDiameter(root.right);
+		int sd = height(root.left) + height(root.right) + 2;
+		return Math.max(sd, Math.max(rd, ld));
+	}
 }
