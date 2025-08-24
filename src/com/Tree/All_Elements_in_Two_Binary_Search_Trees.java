4ccf57dca47356e4e60cb47a5d5b1ea0dff4d440
sidra <sidrajagrawal@gmail.com>
2025-08-24 11:21:28 +0530

    New

--- src/com/Tree/All_Elements_in_Two_Binary_Search_Trees.java
+++ src/com/Tree/All_Elements_in_Two_Binary_Search_Trees.java
@@ -23,36 +23,28 @@ public class All_Elements_in_Two_Binary_Search_Trees {
 
 	class Solution {
 		public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-			List<Integer> lst = new ArrayList<>();
-			inorder(root1,root2,lst);
+			List<Integer> lst1 = new ArrayList<>();
+			List<Integer> lst2 = new ArrayList<>();
+			List<Integer> ans = new ArrayList<>();
+			inorder(root1,lst1);
+			inorder(root2,lst2);
+			int i=0,j=0;
+			while(i<lst1.size() && j<lst2.size()) {
+				if(lst1.get(i) < lst2.get(j)) {
+					ans.add(lst1.get(i++));
+				}else if(lst1.get(j) < lst2.get(i)) {
+					ans.add(lst1.get(i++));
+				}
+			}
 		}
 
-		private void inorder(TreeNode t1, TreeNode t2,List<Integer> lst) {
+		private void inorder(TreeNode root,List<Integer> lst) {
 			// TODO Auto-generated method stub
-			if(t1 == null && t2 == null) return ;
+			if(root == null) return ;
 			
-			if(t1 == null && t2 != null) {
-				inorder(t1.left,t2,lst);
-			}if(t1 != null && t2 ==null) {
-				inorder(t1,t2.left,lst);
-			}else{
-				inorder(t1.left,t2.left,lst);
-			}
-			if(t1.val < t2.val) {
-				lst.add(t1.val);
-				lst.add(t2.val);
-			}
-			else {
-				lst.add(t2.val);
-				lst.add(t1.val);
-			}
-			if(t1 == null && t2 != null) {
-				inorder(t1.right,t2,lst);
-			}if(t1 != null && t2 ==null) {
-				inorder(t1,t2.right,lst);
-			}else{
-				inorder(t1.right,t2.right,lst);
-			}
+			inorder(root.left,lst);
+			lst.add(root.val);
+			inorder(root.right,lst);
 		}
 	}
 }
