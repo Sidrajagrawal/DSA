c1666188a1c52b07fe3891263b8f18691ff34eda
sidra <sidrajagrawal@gmail.com>
2025-08-27 12:47:42 +0530

    new

---
+++ src/Map/HashMapClient.java
@@ -0,0 +1,15 @@
+package Map;
+
+public class HashMapClient {
+	public static void main(String[] args) {
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("raj", 76);
+		map.put("abcd", 36);
+		map.put("kunal",98);
+		map.put("puneet", 86);
+		map.put("saksham", 99);
+		map.put("sidbaby", 87);
+		map.put("dham dham", 65);
+		System.out.println(map);
+	}
+}
--- src/com/Tree/Trim_a_Binary_Search_Tree.java
+++ src/com/Tree/Trim_a_Binary_Search_Tree.java
@@ -1,5 +1,44 @@
 package com.Tree;
 
+import java.util.*;
+
 public class Trim_a_Binary_Search_Tree {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+	    public TreeNode trimBST(TreeNode root, int low, int high) {
+	        if (root == null) return null;
+
+	        if (root.val < low) {
+	            return trimBST(root.right, low, high);
+	        }
+
+	        if (root.val > high) {
+	            return trimBST(root.left, low, high);
+	        }
+
+	        root.left = trimBST(root.left, low, high);
+	        root.right = trimBST(root.right, low, high);
+
+	        return root;
+	    }
+	}
 
 }
