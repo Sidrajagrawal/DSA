a7ff79662270efe65756ba04c1b56f6e074c89d4
sidra <sidrajagrawal@gmail.com>
2025-08-28 12:40:23 +0530

    Fibonacci Dp

--- src/dynamic_programming/FibonacciWithDp.java
+++ src/dynamic_programming/FibonacciWithDp.java
@@ -1,35 +1,46 @@
 package dynamic_programming;
 
 public class FibonacciWithDp {
-	public static void main(String args[]) {
-		int n=5;
-		int[] dp = new int[n+1];
-		System.out.println(fib(n));
-		System.out.println(fibTd(n,dp));
-	}
-	private static int fibTd(int n,int []dp) {
-		// TODO Auto-generated method stub
-		if(n == 0 || n==1) {
-			return n;
-		}
-		
-		if(dp[n]!=0) {
-			return dp[n];
-		}
-		
-		int f1 = fibTd(n-1,dp);
-		int f2 = fibTd(n-1,dp);
-		
-		return dp[n] = f1+f2;
+    public static void main(String args[]) {
+        int n = 5;
+        int[] dp = new int[n + 1];
+        System.out.println("Normal Recursion: " + fib(n));
+        System.out.println("Top-Down DP: " + fibTd(n, dp));
+        System.out.println("Bottom-Up DP: " + fibBu(n));
+    }
+
+    private static int fibBu(int n) {
+    	int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<dp.length; i++) {
+        	dp[i] = dp[i-1] + dp[i-2];
+        }
+		return dp[n];
 	}
 
-	private static int fib(int n) {
-		// TODO Auto-generated method stub
-		if(n == 0 || n==1) {
-			return n;
-		}
-		int f1 = fib(n-1);
-		int f2 = fib(n-1);
-		return f1+f2;
-	}
+	// Top-down with memoization (DP)
+    private static int fibTd(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        int f1 = fibTd(n - 1, dp);
+        int f2 = fibTd(n - 2, dp);  
+        return dp[n] = f1 + f2;
+    }
+
+    // Simple recursion (without DP)
+    private static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int f1 = fib(n - 1);
+        int f2 = fib(n - 2);  
+        return f1 + f2;
+    }
 }
