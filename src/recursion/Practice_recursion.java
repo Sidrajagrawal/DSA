81f58a37719b84cf4ee230156299041afe563d26
sidra <sidrajagrawal@gmail.com>
2025-08-30 12:02:45 +0530

    practice4

--- src/recursion/Practice_recursion.java
+++ src/recursion/Practice_recursion.java
@@ -3,17 +3,13 @@ import java.util.*;
 
 public class Practice_recursion {
 	public static void main(String args[]) {
-		int[] arr = {2,31,4,5,6,3,7};
-		int key = 3;
-		System.out.println(search(arr,key,0));
+		int n = 6;
+		System.out.println(fibo(n));
 	}
 
-	private static int search(int[] arr,int key,int idx) {
-		if(idx == arr.length) return -1;
+	private static int fibo(int n) {
+		if(n == 0 || n == 1) return n;
 		
-		if(arr[idx] == key ) return idx;
-		
-		int res = search(arr,key,idx+1);
-		return res;
+		return fibo(n-1) + fibo(n-2);
 	}
 }
