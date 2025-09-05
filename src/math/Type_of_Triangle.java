35eab05b27a85e3afd0b1da89dc8ef51c5af206b
sidra <sidrajagrawal@gmail.com>
2025-09-05 23:55:03 +0530

    New

--- src/math/Type_of_Triangle.java
+++ src/math/Type_of_Triangle.java
@@ -1,5 +1,20 @@
 package math;
 
 public class Type_of_Triangle {
-
+	class Solution {
+	    public String triangleType(int[] nums) {
+	    	int a = nums[0];
+	    	int b = nums[1];
+	    	int c = nums[2];
+	    	if(a+b <= c || b+c <= a || c+a <=b) {
+	    		return "none";
+	    	}else if(a == b && b==c) {
+	    		return "equilateral";
+	    	}else if(a==b || b==c || a==c) {
+	    		return "isosceles";
+	    	}
+	    	return "scalene";
+	    }
+	}
 }
+
