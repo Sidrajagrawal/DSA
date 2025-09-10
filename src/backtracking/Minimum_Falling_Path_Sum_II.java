f87ae5d91ed5cf52aa2d8560edc4fd15030efdad
sidra <sidrajagrawal@gmail.com>
2025-09-10 12:41:01 +0530

    09Sept2025

--- src/backtracking/Minimum_Falling_Path_Sum_II.java
+++ src/backtracking/Minimum_Falling_Path_Sum_II.java
@@ -1,5 +1,31 @@
 package backtracking;
 
-public class Minimum_Falling_Path_Sum_II {
+import java.util.Arrays;
 
+public class Minimum_Falling_Path_Sum_II {
+	class Solution {
+	    public int minFallingPathSum(int[][] grid) {
+	       int ans=Integer.MAX_VALUE;
+	        int[][] dp=new int[grid.length][grid.length];
+	        for(int[] arr:dp){
+	            Arrays.fill(arr,-10001);
+	        }
+	        for(int col=0;col<grid[0].length;col++){
+	            ans=Math.min(ans,MinPath(grid,0,col,dp));
+	        }
+	        return ans;
+	    }
+	    public int MinPath(int[][] matrix,int cr,int cc,int[][] dp){
+	        if(cr==matrix.length-1){
+	            return matrix[cr][cc];
+	        }
+	        if(dp[cr][cc]!=-10001) return dp[cr][cc]; 
+	        int ans=Integer.MAX_VALUE;
+	        for(int i=0;i<matrix[0].length;i++){
+	            if(i==cc) continue;
+	            ans=Math.min(ans,MinPath(matrix,cr+1,i,dp));
+	        }
+	        return dp[cr][cc]=ans+matrix[cr][cc];
+	    }
+	}
 }
