46023c53f06fbd3afec190c4c3c7e3ee194cae9e
sidra <sidrajagrawal@gmail.com>
2025-09-13 23:03:46 +0530

    rotate

---
+++ src/Array/Rotate_Array.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class Rotate_Array {
+	class Solution {
+	    public void rotate(int[] nums, int k) {
+	        int n = nums.length;
+	        k = k%n;
+	        rev(nums, 0 , n-1);
+	        rev(nums, 0 , k-1);
+	        rev(nums, k, n-1);
+	    }
+
+		private void rev(int[] nums, int i, int j) {
+			// TODO Auto-generated method stub
+			while(i<j) {
+				int temp = nums[i];
+				nums[i++] = nums[j];
+				nums[j--] = temp;
+			}
+		}
+	}
+}
