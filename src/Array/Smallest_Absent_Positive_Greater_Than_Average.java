35b72118f3d3314e899d364e7602ea24ba8c4d44
sidra <sidrajagrawal@gmail.com>
2025-09-13 20:13:41 +0530

    Biweekly

---
+++ src/Array/Smallest_Absent_Positive_Greater_Than_Average.java
@@ -0,0 +1,30 @@
+package Array;
+
+public class Smallest_Absent_Positive_Greater_Than_Average {
+	class Solution {
+	    public int smallestAbsent(int[] nums) {
+	        int sum = 0, max = Integer.MIN_VALUE;
+	        for(int i=0;i<nums.length;i++){
+	            max = Math.max(max,nums[i]);
+	            sum += nums[i];
+	        }
+	        int avg = sum/nums.length,ans = 0;
+	        for(int i=1;i<105;i++){
+	            boolean flag = true;
+	            for(int j=0;j<nums.length;j++){
+	                if(nums[j] == i){
+	                    flag = false;
+	                    break;
+	                }
+	            }
+	            if(flag){
+	                if(i > avg){
+	                    ans = i;
+	                    break;
+	                }
+	            }
+	        }
+	        return ans;
+	    }
+	}
+}
