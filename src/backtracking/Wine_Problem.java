7adfa1276784b6c134fba0ed3f8d0ca0e547bfb9
sidra <sidrajagrawal@gmail.com>
2025-09-18 11:09:21 +0530

    MCM

--- src/backtracking/Wine_Problem.java
+++ src/backtracking/Wine_Problem.java
@@ -3,15 +3,38 @@ package backtracking;
 public class Wine_Problem {
 	public static void main(String[] args) {
 		int[] wine = { 2, 3, 5, 1, 4 };
-		System.out.println(maximum_profit(wine, 0, wine.length - 1, 1));
+		int[][] dp = new int[wine.length][wine.length];
+//		System.out.println(maximum_profitTD(wine, 0, wine.length - 1, 1,dp));
+		System.out.println(maximum_profitBU(wine));
 	}
-
-	public static int maximum_profit(int[] wine, int i, int j, int year) {
+	
+	public static int maximum_profitBU(int[] wines) {
+		int[][] dp = new int[wines.length][wines.length];
+		int year = wines.length;
+		for(int i=0; i<dp.length; i++) {
+			dp[i][i] = wines[i] * year;
+		}
+		year--;
+		for(int gap=1; gap<dp.length; gap++) {
+			for(int j=gap; j<dp.length; j++) {
+				int i = j-gap;
+				int fs = wines[i] * year + dp[i-1][j];
+				int ls = wines[j] * year + dp[i][j-1]; 
+			}
+			year--;
+		}
+		return dp[0][dp.length-1];
+	}
+	
+	public static int maximum_profitTD(int[] wine, int i, int j, int year,int[][]dp) {
 		if (i > j) {
 			return 0;
 		}
-		int fs = wine[i] * year + maximum_profit(wine, i + 1, j, year + 1);
-		int ls = wine[j] * year + maximum_profit(wine, i, j - 1, year + 1);
-		return Math.max(fs, ls);
+		if(dp[i][j] != 0) {
+			return dp[i][j];
+		}
+		int fs = wine[i] * year + maximum_profitTD(wine, i + 1, j, year + 1,dp);
+		int ls = wine[j] * year + maximum_profitTD(wine, i, j - 1, year + 1,dp);
+		return dp[i][j] = Math.max(fs, ls);
 	}
 }
