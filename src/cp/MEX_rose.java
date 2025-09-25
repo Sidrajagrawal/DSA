23995e2626bb1de54335bdef0c2e9a4c75b5cfa2
sidra <sidrajagrawal@gmail.com>
2025-09-25 22:23:15 +0530

    CP

--- src/cp/MEX_rose.java
+++ src/cp/MEX_rose.java
@@ -1,10 +1,28 @@
 package cp;
 
+import java.util.*;
+
 public class MEX_rose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n=sc.nextInt();
+            int k= sc.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+            int[] freq = new int[n + 1]; 
+            for (int i=0; i<n; i++) {
+                int x = sc.nextInt();
+                if (x>=0 && x<=n) freq[x]++;
+            }
+            int missing = 0;
+            for (int i=0; i<k; i++){
+                if(freq[i]==0) missing++;
+            }
+            int ans;
+            if (k == n) ans = missing;
+            else ans = Math.max(missing,freq[k]);
+            System.out.println(ans);
+        }
+    }
 }
