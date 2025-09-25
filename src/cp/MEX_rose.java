bd12756e814269fe667a5461c1533e0d46ee45f1
sidra <sidrajagrawal@gmail.com>
2025-09-25 20:56:13 +0530

    CP

---
+++ src/cp/MEX_rose.java
@@ -0,0 +1,10 @@
+package cp;
+
+public class MEX_rose {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/cp/Unconventional_Pairs.java
+++ src/cp/Unconventional_Pairs.java
@@ -1,9 +1,28 @@
 package cp;
 
+import java.util.*;
 public class Unconventional_Pairs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = sc.nextInt();
+			}
+			Arrays.sort(arr);
+			int i=0,j=1;
+			int ans = Integer.MIN_VALUE;
+			while(j<arr.length) {
+				int val =  Math.abs(arr[i] - arr[j]);
+				ans = Math.max(ans, val);
+				j=j+2;
+				i=i+2;
+			}
+			System.out.println(ans);
+		}
 
 	}
 
