1d9b4543799af097bcdf6905b1ba29dbb17cc6dc
sidra <sidrajagrawal@gmail.com>
2025-10-24 11:47:31 +0530

    dp

---
+++ src/dynamic_programming/Partition_Equal_Subset_Sum.java
@@ -0,0 +1,23 @@
+package dynamic_programming;
+import java.util.*;
+public class Partition_Equal_Subset_Sum {
+	class Solution {
+	    public boolean canPartition(int[] nums) {
+	        int s=0;
+	        for(int ele: nums) s+=ele;
+	        if(s%2!=0) return false;
+	        int target = s/2;
+	        Boolean[][] dp = new Boolean[nums.length][target+1];
+	        return fun(nums,target,0,dp);
+	    }
+	    public static boolean fun(int[] arr, int target, int idx, Boolean[][] dp){
+	        if(target == 0) return true;
+	        if(idx == arr.length || target < 0) return false;
+	        
+	        if(dp[idx][target] != null) return dp[idx][target];
+	        boolean include = fun(arr, target-arr[idx], idx+1,dp);
+	        boolean exclude = fun(arr, target, idx+1, dp);
+	        return dp[idx][target] = include || exclude;
+	    } 
+	}
+}
